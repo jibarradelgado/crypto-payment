9bee300dbb1cfebc4b2bc4d22ed6ea551aa81b91
Jorge Ibarra <hello@jorgeibarra.com.mx>
2022-01-20 17:06:38 -0600

    Getting data every 2 seconds

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import fetch from 'node-fetch';
 
-function getTicker() {
-  fetch(`https://api.bitso.com/v3/ticker?book=btc_mxn`)
+const getTicker = async () => {
+  await fetch(`https://api.bitso.com/v3/ticker?book=btc_mxn`)
     .then(response => response.json())
     .then(data => console.log(data));
 }
 
-getTicker();
+setInterval(() => {
+  getTicker();
+}, 2000);
